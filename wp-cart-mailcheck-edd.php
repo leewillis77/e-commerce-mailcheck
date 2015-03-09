078e40fb946220d120d2590e27b34309014ca657
Gary Jones <gary@garyjones.co.uk>
2015-03-09 09:09:02 +0000

    Fix suffix addition

--- wp-cart-mailcheck-edd.php
+++ wp-cart-mailcheck-edd.php
@@ -16,7 +16,7 @@ class wpcm_edd {
     function enqueue_scripts() {
     	$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-        wp_enqueue_script ( 'wpcm-edd', plugins_url( 'js/edd{$suffix}.js', __FILE__ ), array ( 'wpcm' ) );
+        wp_enqueue_script ( 'wpcm-edd', plugins_url( "js/edd{$suffix}.js", __FILE__ ), array ( 'wpcm' ) );
 
     }
 
